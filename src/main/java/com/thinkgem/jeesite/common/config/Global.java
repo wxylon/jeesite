be83dc7090615c3f577f62b71ab9996756f5634f
xiong.wang <wxylon@gmail.com>
2016-11-16 13:15:04 +0800

    无关代码删除

--- src/main/java/com/thinkgem/jeesite/common/config/Global.java
+++ src/main/java/com/thinkgem/jeesite/common/config/Global.java
@@ -99,14 +99,6 @@ public class Global {
 	public static String getUrlSuffix() {
 		return getConfig("urlSuffix");
 	}
-
-	/**
-	 * 在修改系统用户和角色时是否同步到Activiti
-	 */
-	public static Boolean isSynActivitiIndetity() {
-		String dm = getConfig("activiti.isSynActivitiIndetity");
-		return "true".equals(dm) || "1".equals(dm);
-	}
     
 	/**
 	 * 页面获取常量
